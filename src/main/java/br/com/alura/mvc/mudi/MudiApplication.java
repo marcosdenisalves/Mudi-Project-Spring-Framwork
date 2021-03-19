58b7e32fc0fb86ff92f0a3cdad5e3ccff55bf9f1
Study Profile <dee.alves2@gmail.com>
2021-03-18 23:04:24 -0300

    Integração com Spring-data-jpa

--- src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
+++ src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
@@ -1,13 +1,38 @@
 package br.com.alura.mvc.mudi;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repositories.PedidoRepository;
+
 @SpringBootApplication
-public class MudiApplication {
+public class MudiApplication implements CommandLineRunner {
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MudiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Pedido pedido = new Pedido();
+		pedido.setNome("Xiaomi Redmi Note 8");
+		pedido.setUrlImagem("https://http2.mlstatic.com/D_NQ_NP_2X_920977-MLA43915525858_102020-F.webp");
+		pedido.setUrlProduto(
+				"https://produto.mercadolivre.com.br/MLB-1726708542-xiaomi-redmi-note-8-dual-sim-64-gb-neptune-blue-4-gb-ram-_JM?matt_tool=84274024&matt_word=&matt_source=google&matt_campaign_id=12406821329&matt_ad_group_id=113343067330&matt_match_type=&matt_network=g&matt_device=c&matt_creative=500403102840&matt_keyword=&matt_ad_position=&matt_ad_type=pla&matt_merchant_id=283323489&matt_product_id=MLB1726708542&matt_product_partition_id=358100206005&matt_target_id=pla-358100206005&gclid=Cj0KCQjwrsGCBhD1ARIsALILBYqZpvXPXziukVnvlpd7uuqjUi2iK98hu3m1SoSNqnmRUy9phzObeDMaAntQEALw_wcB#&gid=1&pid=1");
+		pedido.setDescricao("Uma descrição qualquer para este pedido");
+
+		List<Pedido> pedidos = pedidoRepository.findAll();
+
+		if (pedidos.isEmpty()) {
+			pedidoRepository.save(pedido);
+		} 
+	}
 }
--- src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -3,14 +3,52 @@ package br.com.alura.mvc.mudi.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedido")
 public class Pedido {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
 	private String nome;
 	private BigDecimal valorNegociado;
 	private LocalDate dataEntrega;
+	
+	@Column(length = 800)
 	private String urlProduto;
+	
 	private String urlImagem;
 	private String descricao;
+
+	public Pedido() {
+	}
+	
+	public Pedido(Integer id, String nome, BigDecimal valorNegociado, LocalDate dataEntrega, String urlProduto,
+			String urlImagem, String descricao) {
+		this.id = id;
+		this.nome = nome;
+		this.valorNegociado = valorNegociado;
+		this.dataEntrega = dataEntrega;
+		this.urlProduto = urlProduto;
+		this.urlImagem = urlImagem;
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
 	public String getNome() {
 		return nome;
