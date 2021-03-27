fc9344b7f11aba938d60cda7eb48c4882a2c62be
Study Profile <dee.alves2@gmail.com>
2021-03-27 20:09:05 -0300

    Adicionando validações no PedidoController

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -1,12 +1,15 @@
 package br.com.alura.mvc.mudi.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import br.com.alura.mvc.mudi.dto.RequisicaoNovoPedido;
+import br.com.alura.mvc.mudi.dto.PedidoDTO;
 import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.repositories.PedidoRepository;
 
@@ -18,12 +21,16 @@ public class PedidoController {
 	private PedidoRepository repository;
 	
 	@GetMapping("formulario")
-	public String formulario() {
+	public String formulario(PedidoDTO requisicao) {
 		return "pedido/formulario";
 	}
 	
 	@PostMapping("novo")
-	public String novo(RequisicaoNovoPedido requisicao) {
+	public String novo(@Valid PedidoDTO requisicao, BindingResult result) {
+		if(result.hasErrors()) {
+			return "pedido/formulario";
+		}
+		
 		Pedido pedido = requisicao.toPedido();
 		repository.save(pedido);
 		return "pedido/formulario";
