31d2b321ff4bce6178b82e7eaea9a6f14c19d596
Study Profile <dee.alves2@gmail.com>
2021-03-27 21:21:39 -0300

    Criando derived query para status

--- src/main/java/br/com/alura/mvc/mudi/repositories/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repositories/PedidoRepository.java
@@ -1,10 +1,15 @@
 package br.com.alura.mvc.mudi.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.enums.StatusPedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+
+	List<Pedido> findByStatus(StatusPedido aguardando);
 }
