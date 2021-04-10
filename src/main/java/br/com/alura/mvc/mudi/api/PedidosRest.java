f289f1df987515e3a0000073a00b9f2d4a8ea349
Study Profile <dee.alves2@gmail.com>
2021-04-10 19:52:09 -0300

    trabalhando com Ajax e Vue.js

---
+++ src/main/java/br/com/alura/mvc/mudi/api/PedidosRest.java
@@ -0,0 +1,31 @@
+package br.com.alura.mvc.mudi.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.enums.StatusPedido;
+import br.com.alura.mvc.mudi.repositories.PedidoRepository;
+
+@RestController
+@RequestMapping("/api/pedidos")
+public class PedidosRest {
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@GetMapping("aguardando")
+	public List<Pedido> getPedidosAguardandoOfertas() {
+		Sort sort = Sort.by("id").descending();
+//		return pedidoRepository.findByStatus(StatusPedido.AGUARDANDO,
+//				PageRequest.of(0, 10, sort));
+
+		return pedidoRepository.findAll();
+	}
+}
--- src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -2,7 +2,9 @@ package br.com.alura.mvc.mudi.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -12,6 +14,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import br.com.alura.mvc.mudi.model.enums.StatusPedido;
@@ -40,6 +43,9 @@ public class Pedido {
 	@ManyToOne(fetch = FetchType.LAZY)
 	private User user;
 
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pedido", fetch = FetchType.LAZY)
+	private List<Oferta> ofertas;
+	
 	public Pedido() {
 	}
 
@@ -124,5 +130,13 @@ public class Pedido {
 
 	public void setUser(User user) {
 		this.user = user;
+	}
+
+	public List<Oferta> getOfertas() {
+		return ofertas;
+	}
+
+	public void setOfertas(List<Oferta> ofertas) {
+		this.ofertas = ofertas;
 	}
 }
